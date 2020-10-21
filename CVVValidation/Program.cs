560680a625f06c05d4f2b2dba619140f162dd12a
Opilane TTHK <opilane@tthk.ee>
2020-10-21 16:04:01 +0300

    first commit

---
+++ CVVValidation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CVVValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your cvv card number: ");
+            //int code = Int32.Parse(Console.ReadLine());
+            
+            string cvv = Console.ReadLine();
+            Console.WriteLine($"Your ccv {cvv}");
+
+            if (Num(cvv))
+            {
+                Console.WriteLine("All good");
+            }
+
+        }
+
+        public static bool Num(string cvv)
+        {
+            if(cvv.Length == 3)
+            {
+                try
+                {
+                    Int32.Parse(cvv);
+                    return true;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"wrong format {e}");
+                    return false;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("wrong format");
+                return false;
+            }
+        }
+
+        
+
+    }
+
+}
+
